c80be1f50ad5696105deea269177e77e561bdb85
stone <aa86799@163.com>
2024-10-29 08:18:24 +0800

    抽奖作业 work_day01_lottery.py

--- lufei/day1/work_day01_lottery.py
+++ lufei/day1/work_day01_lottery.py
@@ -2,7 +2,7 @@
 # 奖项如下:路飞科技有限公司有300员工，开年会抽奖，
 # 一等奖 3名:泰国5日游+手术费报销
 # 二等奖 6名:iPhone16手机
-# 三斤苹果三等奖30名:
+# 三等奖30名: ...
 # 规则:
 # 1.共抽3次，第一次抽3等奖，第2次抽2等奖，第3次压轴抽1等奖
 # 2.每个员工限中奖一次，不能重复
@@ -14,13 +14,15 @@
 import random
 
 
-def lottery_detail(employs, del_key_list, awards_count):
+def lottery_detail(employs, del_key_list, awards_count, grade):
+    times = 0
     while len(del_key_list) < awards_count:
         rand_item_key = random.choice(list(employs.keys()))
         if rand_item_key in del_key_list:  # 已包含key， 进行下次循环
             continue
         rand_item_value = employs[rand_item_key]
-        print(f"三等奖抽中编号 {rand_item_key}")
+        times += 1
+        print(f"第{times}次抽，{grade}抽中编号: {rand_item_key}")
         del_key_list.append(rand_item_key)
     for key in del_key_list:
         del employs[key]
@@ -30,12 +32,11 @@ def lottery_detail(employs, del_key_list, awards_count):
 def lottery():
     employs_count = 300
     prefix_str = "lufei_"
-    employs = {}  # 员工编号字典列表
+    employs = {}  # 员工编号字典
     for i in range(employs_count):
         employs[prefix_str + str(i)] = i
-
-    print(type(employs))
-    print(employs)
+    # print(type(employs))
+    # print(employs)
 
     awards3 = 30  # 三等奖
     awards2 = 6  # 二等奖
@@ -43,13 +44,13 @@ def lottery():
     del_key_list = []  # 待删除员工key列表
 
     print("-----开始抽三等奖------")
-    lottery_detail(employs, del_key_list, awards3)
+    lottery_detail(employs, del_key_list, awards3, "三等奖")
 
     print("-----开始抽二等奖------")
-    lottery_detail(employs, del_key_list, awards2)
+    lottery_detail(employs, del_key_list, awards2, "二等奖")
 
     print("-----开始抽一等奖------")
-    lottery_detail(employs, del_key_list, awards1)
+    lottery_detail(employs, del_key_list, awards1, "一等奖")
 
 
 if __name__ == '__main__':
