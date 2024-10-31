8198f85724759cce745d2e36ab45398473953018
stone <aa86799@163.com>
2024-10-31 09:48:31 +0800

    improve 炸金花

--- lufei/golden_flower.py
+++ lufei/golden_flower.py
@@ -10,6 +10,7 @@
 # 1.先生成一付完整的扑克牌
 # 2.给5个玩家随机发牌
 # 3.统一开牌，比大小，输出赢家是谁
+# 4. 允许 点数一到处，但不同花色各牌型 大小一致。eg. ♠️2/3/4 平 ♥️2/3/4
 
 import random
 
@@ -24,17 +25,36 @@ pokers_ch = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 pokers_color = ['♠️', '♥️', '♣️', '♦️']
 
 
+def poker_mode_desc(pokers_mode):
+    if pokers_mode == 6:
+        result = "豹子"
+    elif pokers_mode == 5:
+        result = "同花顺"
+    elif pokers_mode == 4:
+        result = "同花"
+    elif pokers_mode == 3:
+        result = "顺子"
+    elif pokers_mode == 2:
+        result = "对子"
+    else:
+        result = "单张"
+    return result
+
+
 class Player:
     def __init__(self, name, pokers, pokers_mode):
-        self.name = name
-        self.pokers = pokers
-        self.pokers_mode = pokers_mode
+        self.name = name  # 玩家名
+        self.pokers = pokers  # 持有的三张牌列表
+        self.pokers_mode = pokers_mode  # 牌型
 
 
 class Poker:
-    def __init__(self, color, char):
-        self.color = color
-        self.char = char
+    """单张牌模型"""
+
+    def __init__(self, color, char, char_index):
+        self.color = color  # 花色
+        self.char = char  # 点数
+        self.char_index = char_index  # 点数索引
 
     def __str__(self):
         return self.color + self.char
@@ -48,10 +68,10 @@ def swap(players, i, j):
 
 def compare(players):
     print("------牌型大小排序后------")
-    new_players = sorted(players, key=lambda p: p.pokers_mode)
+    new_players = sorted(players, key=lambda p: p.pokers_mode)  # 玩家对象 按牌型排序
     for item in new_players:
-        item.pokers = sorted(item.pokers, key=lambda p: pokers_ch.index(p.char))
-        print(f"玩家{item.name}", item.pokers_mode, display(item.pokers))
+        item.pokers = sorted(item.pokers, key=lambda p: p.char_index)  # 单玩家手中牌  按点数字符的索引排序
+        print(f"玩家-{item.name}，", f"牌型-{poker_mode_desc(item.pokers_mode)}，", display(item.pokers))
     for i in range(0, len(new_players)):
         if i == 0:
             continue
@@ -61,88 +81,88 @@ def compare(players):
             swap(new_players, i, i - 1)
         else:  # 后面的牌型不大，即 相等
             if new_players[i].pokers_mode in [豹子, 同花顺, 顺子]:  # 内部比大小规则一致的
-                if pokers_ch.index(new_players[i].pokers[0].char) < pokers_ch.index(new_players[i - 1].pokers[0].char):
-                    swap(new_players, i, i-1)
+                if new_players[i].pokers[0].char_index < new_players[i - 1].pokers[0].char_index:
+                    swap(new_players, i, i - 1)
             elif new_players[i].pokers_mode == 对子:
-                i_duizi_char = ""
-                i_single_char = ""
+                i_duizi_char_index = -1
+                i_single_char_index = -1
                 if new_players[i].pokers[0].char == new_players[i].pokers[1].char:
-                    i_duizi_char = new_players[i].pokers[0].char
-                    i_single_char = new_players[i].pokers[2].char
+                    i_duizi_char_index = new_players[i].pokers[0].char_index
+                    i_single_char_index = new_players[i].pokers[2].char_index
                 else:
-                    i_duizi_char = new_players[i].pokers[1].char
-                    i_single_char = new_players[i].pokers[0].char
-                last_duizi_char = ""
-                last_single_char = ""
+                    i_duizi_char_index = new_players[i].pokers[1].char_index
+                    i_single_char_index = new_players[i].pokers[0].char_index
+                last_duizi_char_index = -1
+                last_single_char_index = -1
                 if new_players[i - 1].pokers[0].char == new_players[i - 1].pokers[1].char:
-                    last_duizi_char = new_players[i - 1].pokers[0].char
-                    last_single_char = new_players[i - 1].pokers[2].char
+                    last_duizi_char_index = new_players[i - 1].pokers[0].char_index
+                    last_single_char_index = new_players[i - 1].pokers[2].char_index
                 else:
-                    last_duizi_char = new_players[i - 1].pokers[1].char
-                    last_single_char = new_players[i - 1].pokers[0].char
-                if pokers_ch.index(i_duizi_char) < pokers_ch.index(last_duizi_char):
-                    swap(new_players, i, i-1)
-                elif pokers_ch.index(i_duizi_char) == pokers_ch.index(last_duizi_char):
-                    if pokers_ch.index(i_single_char) < pokers_ch.index(last_single_char):
-                        swap(new_players, i, i-1)
+                    last_duizi_char_index = new_players[i - 1].pokers[1].char_index
+                    last_single_char_index = new_players[i - 1].pokers[0].char_index
+                if i_duizi_char_index < last_duizi_char_index:
+                    swap(new_players, i, i - 1)
+                elif i_duizi_char_index == last_duizi_char_index:
+                    if i_single_char_index < last_single_char_index:
+                        swap(new_players, i, i - 1)
             else:  # 同花、散张，内部比大小规则一致的
-                if pokers_ch.index(new_players[i].pokers[-1].char) < pokers_ch.index(new_players[i - 1].pokers[-1].char):
-                    swap(new_players, i, i-1)
-                elif pokers_ch.index(new_players[i].pokers[-1].char) == pokers_ch.index(new_players[i - 1].pokers[-1].char):
-                    if pokers_ch.index(new_players[i].pokers[-2].char) < pokers_ch.index(new_players[i - 1].pokers[-2].char):
-                        swap(new_players, i, i-1)
-                    elif pokers_ch.index(new_players[i].pokers[-2].char) == pokers_ch.index(new_players[i - 1].pokers[-2].char):
-                        if pokers_ch.index(new_players[i].pokers[-3].char) < pokers_ch.index(new_players[i - 1].pokers[-3].char):
-                            swap(new_players, i, i-1)
+                if new_players[i].pokers[-1].char_index < new_players[i - 1].pokers[-1].char_index:
+                    swap(new_players, i, i - 1)
+                elif new_players[i].pokers[-1].char_index == new_players[i - 1].pokers[-1].char_index:
+                    if new_players[i].pokers[-2].char_index < new_players[i - 1].pokers[-2].char_index:
+                        swap(new_players, i, i - 1)
+                    elif new_players[i].pokers[-2].char_index == new_players[i - 1].pokers[-2].char_index:
+                        if new_players[i].pokers[-3].char_index < new_players[i - 1].pokers[-3].char_index:
+                            swap(new_players, i, i - 1)
     return new_players
 
+
 def is_baozi(pokers):
-    # flag = True
-    # for i in range(0, len(pokers)):
-    #     if i == 0:
-    #         continue
-    #     if pokers[i].char != pokers[i - 1].char:
-    #         flag = False
-    #         continue
-    # return flag
+    """
+    三个点数是否一致
+    """
     return pokers[0].char == pokers[1].char and pokers[0].char == pokers[2].char
 
 
 def is_tonghua(pokers):
-    # flag = True
-    # for i in range(0, len(pokers)):
-    #     if i == 0:
-    #         continue
-    #     if pokers[i].color != pokers[i - 1].color:
-    #         flag = False
-    #         continue
-    # return flag
+    """
+    三个花色是否一致
+    """
     return pokers[0].color == pokers[1].color and pokers[0].color == pokers[2].color
 
 
 def is_shunzi(pokers):
+    """
+    先按点数字符索引排序。判断是否是特殊顺子牌型 23A；循环判断后一位的索引减去前一位的索引的差值是否为1.
+    """
     flag = True
-    pokers = sorted(pokers, key=lambda p: pokers_ch.index(p.char))  # 排序
+    pokers = sorted(pokers, key=lambda p: p.char_index)  # 排序
     # 特殊的顺子牌型顺序 [2, 3, A]
     if pokers[0].char == pokers_ch[0] and pokers[1].char == pokers_ch[1] and pokers[2].char == pokers_ch[-1]:
         return flag
     for i in range(0, len(pokers)):
         if i == 0:
             continue
-        if pokers_ch.index(pokers[i].char) - pokers_ch.index(pokers[i - 1].char) != 1:
+        if pokers[i].char_index - pokers[i - 1].char_index != 1:
             flag = False
-            continue
+            break
     return flag
 
 
 def is_duizi(pokers):
-    pokers = sorted(pokers, key=lambda p: pokers_ch.index(p.char))  # 排序
+    """
+    先按点数字符索引排序。前两个点数或后两个点数是否一致
+    """
+    pokers = sorted(pokers, key=lambda p: p.char_index)  # 排序
     if pokers[0].char == pokers[1].char or pokers[1].char == pokers[2].char:
         return True
     return False
 
 
 def is_tonghuashui(pokers):
+    """
+    是否 既是同花且是顺子
+    """
     return is_tonghua(pokers) and is_shunzi(pokers)
 
 
@@ -175,18 +195,18 @@ def gambling():
     print("------开始生成一幅牌------")
 
     pokers = []
-    for ch in pokers_ch:
-        for color in pokers_color:
-            pokers.append(Poker(color, ch))
+    for ch in pokers_ch:  # 13个字符点数
+        for color in pokers_color:  # 4种花色
+            pokers.append(Poker(color, ch, pokers_ch.index(ch)))
     print(display(pokers))
 
     print("------洗牌后------")
-    pokers_count = len(pokers)  # 整幅牌数量
+    pokers_count = len(pokers)  # 整幅牌数量 13*4=52
     pokers = random.sample(pokers, pokers_count)
     print(display(pokers))
 
     print("------切牌------")
-    players_count = 5  # 总玩家数
+    players_count = 10  # 总玩家数
     every_players_pokers_count = 3  # 每人发三张
     players = []
     for i in range(0, players_count):
@@ -202,10 +222,10 @@ def gambling():
             if i == 2:
                 players[j].pokers_mode = mode_grade(players[j].pokers)
             random_index += 1
-            if random_index >= pokers_count - 1:
+            if random_index == pokers_count:
                 random_index = 0  # 归零
     for item in players:
-        print(item.pokers_mode, display(item.pokers))
+        print(f"玩家-{item.name}，", f"牌型-{poker_mode_desc(item.pokers_mode)}，", display(item.pokers))
 
     print("------比大小------")
     # grade = mode_grade(players_pokers[0])
@@ -227,13 +247,36 @@ def gambling():
     # for item in mode_list:
     #     display(item.pokers)
 
+    players.append(Player("11", [Poker('♣️', 'J', 0), Poker('♠️', 'Q', 2), Poker('♣️', 'K', 12)], 3))
+    players.append(Player("12", [Poker('♥️', 'J', 0), Poker('♦️', 'Q', 2), Poker('♠️', 'K', 12)], 3))
+    players_count += 2
+
     # 因使用内部元素比对后，交换位置的方法，所以需要类似冒泡排序一样，增加一个外部循环，进行多次排序。
     for i in range(0, players_count):
         new_players = compare(players)
         players = new_players
-    print("------比大小，最终结果------")
+    print("------比大小，排序最终结果------")
     for item in players:
-        print(f"玩家{item.name}", item.pokers_mode, display(item.pokers))
+        print(f"玩家-{item.name}，", f"牌型-{poker_mode_desc(item.pokers_mode)}，", display(item.pokers))
+    print("------比大小，最大结果------")
+    step = -1
+    for i in range(0, players_count):
+        if i == 0:  # step - i 即 [-1] ，肯定是属于最大的其一
+            print(f"玩家-{players[step + i].name}，", f"牌型-{poker_mode_desc(players[step + i].pokers_mode)}，",
+                  display(players[step + i].pokers))
+            step -= 2
+            continue
+        cur = players[step + i]
+        pre = players[step + i + 1]
+        # 后面的 每一位字符和前面的每一位相同，则一样大
+        if cur.pokers_mode == pre.pokers_mode and cur.pokers[0].char == pre.pokers[0].char \
+                and cur.pokers[1].char == pre.pokers[1].char and cur.pokers[2].char == pre.pokers[2].char:
+            print(f"玩家-{players[step + i].name}，", f"牌型-{poker_mode_desc(players[step + i].pokers_mode)}，",
+                  display(players[step + i].pokers))
+            step -= 2
+        else:
+            break
+
 
 if __name__ == "__main__":
     gambling()
